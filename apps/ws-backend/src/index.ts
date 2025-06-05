ff49daf1a17133de9a8bd0b9377a5879ae468f3f
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-06-05 09:19:41 +0530

    ws added

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,52 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+interface User {
+  ws : WebSocket,
+  rooms : string[],
+  userId : string
+}
+
+const users:User[] = [];
+wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
+  const url = request.url;
+  if(!url){
+    console.log("url not found");
+    return;
+  }
+  const params = new URLSearchParams(url.split("?")[1]);
+  const token = params.get("token")
+  if(!token){
+    console.log("token not found");
+    return;
+  }
+  const validate = jwt.verify(token, JWT_SECRET);
+
+  if( typeof validate === "string"){
+    return ;
+  }
+  if(!validate || !validate.userId){
+    console.log("token is not validate");
+    return ;
+  }
+  const userId = validate.userId ;
+
+  if(userId === null){
+    ws.close();
+    return;
+  }
+   users.push({
+    userId,
+    rooms : [],
+    ws
+   })
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log('received: ', data);
   });
 
   ws.send('pong');
