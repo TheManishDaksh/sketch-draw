0436feff715f1c88c1169d008d3357d902eba0b9
TheManishDaksh <140195332+TheManishDaksh@users.noreply.github.com>
2025-06-05 12:59:44 +0530

    db added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,32 +1,67 @@
 import { userSignin, userSignup } from "@repo/zod/types";
-import express from "express";
-import { Jwt } from "jsonwebtoken";
+import express, { Request, Response } from "express";
 import { middleware } from "./middleware";
+import { prisma } from "@repo/db/client";
 
 const app = express();
 app.use(express.json());
 
-app.post("signup", (req, res)=>{
-    const parsedData = userSignup.safeParse(req.body);
+app.post("/signup", async(req:Request, res:Response)=>{
+    const parsedData =  userSignup.safeParse(req.body);
     if(!parsedData){
         res.status(493).json({
             message : "input error"
         })
     }
-    //db integration
+
+   try {
+     const user = await prisma.user.create({
+        data : {
+            name : parsedData.data?.name || "",
+            username : parsedData.data?.username || "",
+            password : parsedData.data?.password || ""
+        }
+    })
+    res.json({userId : user.id})
+   } catch (error: any) {
+    res.status(403).json({
+        message : "user can't create"
+    })
+   }
 })
 
-app.post("signin",middleware, (req, res)=>{
+app.post("/signin",middleware, async(req: Request, res: Response)=>{
     const parsedData = userSignin.safeParse(req.body);
     if(!parsedData){
         res.status(493).json({
             message : "input error"
         })
     }
-    //db find and return name and userid
+    try {
+        const user = await prisma.user.findFirst({
+        where : {
+            username : parsedData.data?.username
+        }
+    })
+    if(!user){
+        res.json({
+            message : "user not found"
+        })
+        return;
+    }
+        res.json({
+            id : user.id,
+            name : user.name,
+            username : user.username
+        })
+    } catch (error:any) {
+       res.status(403).json({
+        message : "user can't signin"
+       }) 
+    }
 })
 
-app.post("close", (req, res)=>{
+app.post("/chat", (req, res)=>{
     
 })
 
